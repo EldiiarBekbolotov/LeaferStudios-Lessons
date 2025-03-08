d44c542dfef7ccdf23437775eadd3da34181b8d3
EldiiarBekbolotov <75341047+EldiiarBekbolotov@users.noreply.github.com>
2025-03-08 17:10:35 -0500

    Update files.js

--- files.js
+++ files.js
@@ -1,1 +1,11 @@
-
+var articles = [{
+	title: "1.0 C++ and Competitive Programming (8 March 2025)",
+	content: "Today, LeaferStudios Lessons has been released to for the Khan Academy community! Visit Lesson 1.1 for more information about the C++ series.",
+	type: "Tutorial",
+}, {
+	title: "1.1 Introduction",
+	content: `C++ is the most common language used in informatics contests such as the International Olympiad in Informatics (IOI), "the most prestigious computer science olympiad for secondary school students". For Khan Academy users who are familiar with web development and JavaScript looking to become better competitive programmers, this means learning C++. JavaScript is not allowed in the IOI.`,
+	type: "Article",
+	by: "LeaferStudios",
+	date: "8 March 2025",
+}];
