c8fad600fb60506327c7822712d1d51c1284a356
EldiiarBekbolotov <75341047+EldiiarBekbolotov@users.noreply.github.com>
2025-03-08 17:33:36 -0500

    Update files.js

--- files.js
+++ files.js
@@ -4,7 +4,7 @@ var articles = [{
 	type: "Tutorial",
 }, {
 	title: "1.1 Introduction",
-	content: `C++ is the most common language used in informatics contests such as the International Olympiad in Informatics (IOI), "the most prestigious computer science olympiad for secondary school students". For Khan Academy users who are familiar with web development and JavaScript looking to become better competitive programmers, this means learning C++. JavaScript is not allowed in the IOI.`,
+	content: `C++ is the most common language used in informatics contests such as the International Olympiad in Informatics (IOI), "the most prestigious computer science olympiad for secondary school students". For Khan Academy users who are familiar with web development and JavaScript looking to become better competitive programmers, this means learning C++. JavaScript is not allowed in the IOI. C++ was first released in 1985, designed by Bjarne Stroustrup, but it had several major updates including one in 2023. If you are familiar with C it will make learning C++ much easier because C++ is an extension of the C language.`,
 	type: "Article",
 	by: "LeaferStudios",
 	date: "8 March 2025",
