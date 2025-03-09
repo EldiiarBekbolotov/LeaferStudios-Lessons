3454211f669baf46845444f79b29eb5885815028
EldiiarBekbolotov <75341047+EldiiarBekbolotov@users.noreply.github.com>
2025-03-08 20:38:35 -0500

    Update files.js

--- files.js
+++ files.js
@@ -9,7 +9,18 @@ var articles = [
 }, 
 {
 	title: "1.1 Introduction",
-	content: `C++ is fast, efficient, and the most common language used in informatics contests such as the International Olympiad in Informatics (IOI), "the most prestigious computer science olympiad for secondary school students.". For Khan Academy users who are familiar with web development and JavaScript and are looking to become better competitive programmers, this means learning C++. JavaScript is not allowed in the IOI. Bjarne Stroustrup first released C++ in 1985, and it underwent several major updates, including one in 2023. If you are familiar with C, it will make learning C++ much easier because C++ is an extension of the C language.`,
+	content: `
+<p>C++ is fast, efficient, and the most common language used in informatics contests such as the International Olympiad in Informatics (IOI), "the most prestigious computer science olympiad for secondary school students.". For Khan Academy users who are familiar with web development and JavaScript and are looking to become better competitive programmers, this means learning C++. JavaScript is not allowed in the IOI. Bjarne Stroustrup first released C++ in 1985, and it underwent several major updates, including one in 2023. If you are familiar with C, it will make learning C++ much easier because C++ is an extension of the C language.</p>
+<h2>The Basics</h2>
+<p><pre><code>#include &lt;iostream&gt
+using namespace std;
+
+int main() {
+	cout &lt;&lt; "Hi";
+	return 0;
+}
+</code></pre></p>
+ `,
 	type: "Article",
 	by: "LeaferStudios",
 	date: "8 March 2025",
