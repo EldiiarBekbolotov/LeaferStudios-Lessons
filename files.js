dcdee5368671febacf0c01d0fd80e8cbc1639213
EldiiarBekbolotov <75341047+EldiiarBekbolotov@users.noreply.github.com>
2025-03-08 20:31:11 -0500

    Update files.js

--- files.js
+++ files.js
@@ -1,11 +1,26 @@
-var articles = [{
+/**
+*	Lesson files for https://leaferstudioslessons.netlify.app/
+**/
+var articles = [
+{
 	title: "1.0 C++ and Competitive Programming (8 March 2025)",
 	content: "Today, LeaferStudios Lessons has been released to for the Khan Academy community! Visit Lesson 1.1 for more information about the C++ series.",
 	type: "Tutorial",
-}, {
+}, 
+{
 	title: "1.1 Introduction",
-	content: `C++ is the most common language used in informatics contests such as the International Olympiad in Informatics (IOI), "the most prestigious computer science olympiad for secondary school students". For Khan Academy users who are familiar with web development and JavaScript looking to become better competitive programmers, this means learning C++. JavaScript is not allowed in the IOI. C++ was first released in 1985, designed by Bjarne Stroustrup, but it had several major updates including one in 2023. If you are familiar with C it will make learning C++ much easier because C++ is an extension of the C language.`,
+	content: `C++ is fast, efficient, and the most common language used in informatics contests such as the International Olympiad in Informatics (IOI), "the most prestigious computer science olympiad for secondary school students.". For Khan Academy users who are familiar with web development and JavaScript and are looking to become better competitive programmers, this means learning C++. JavaScript is not allowed in the IOI. Bjarne Stroustrup first released C++ in 1985, and it underwent several major updates, including one in 2023. If you are familiar with C, it will make learning C++ much easier because C++ is an extension of the C language.`,
 	type: "Article",
 	by: "LeaferStudios",
 	date: "8 March 2025",
-}];
+},
+/*
+{
+	title: "",
+ 	content: ``,
+  	type: "Article",
+   	by: "LeaferStudios",
+    	date: "",
+},
+*/
+];
