5364dcbe008e6ddbfb9cc3603316f146ea9cd9c8
EldiiarBekbolotov <75341047+EldiiarBekbolotov@users.noreply.github.com>
2025-03-11 10:37:57 -0400

    Update files.js

--- files.js
+++ files.js
@@ -2,6 +2,18 @@
 *	Lesson files for https://leaferstudioslessons.netlify.app/
 **/
 var articles = [
+	{
+		title: "2.0 Java Fundamentals (11 March 2025)",
+		content: "The history and fundamentals of the Java language",
+			type: "Tutorial",
+	},
+	{
+		title: "2.1 History",
+ 	content: `FORTRAN, COBOL, and BASIC were either efficient but complex and maintainable, or easy to learn but not efficient.`,
+  	type: "Article",
+   	by: "LeaferStudios",
+    	date: "11 March 2025",
+	},
 {
 	title: "1.0 C++ and Competitive Programming (8 March 2025)",
 	content: "Today, LeaferStudios Lessons has been released to for the Khan Academy community! Visit Lesson 1.1 for more information about the C++ series.",
